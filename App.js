5cc5221a839af78fe9693a5a200a4bce364540e6
Aakash Vishwakarma <akashviahwakarma98@gmail.com>
2022-08-27 14:40:19 +0530

    First Native App repo with screens and navigation config

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import Navigator from './routes/homeStack';
+import Home from './Screens/Home';
+let customFonts = {
+  'Poppins-Black': require('./assets/fonts/Poppins-Black.ttf'),
+  'Poppins-Bold': require('./assets/fonts/Poppins-Bold.ttf')
+};
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+  const [isLoaded] = useFonts(customFonts);
+  if (!isLoaded) {
+    return null
+  }
+    return <Navigator />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ Screens/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { globalStyles } from '../styles/global'
+
+const About = () => {
+  return (
+    <View style={globalStyles.container}>
+        <Text style={globalStyles.titleText}>Home</Text>
+    </View>
+  )
+}
+
+
+export default About
